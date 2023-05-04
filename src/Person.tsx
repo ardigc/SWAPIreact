306be22a4623440b37d3606bfbce1dc774dd9e7b
ardigc <agordo.95@gmail.com>
2023-05-04 14:08:16 +0200

    creando Id en la url

--- src/Person.tsx
+++ src/Person.tsx
@@ -29,18 +29,26 @@ export default function Person(props: People) {
   //         setLoading(false);
   //       });
   //   }, []);
+  function clickHandler() {
+    const ident = url;
+    var regex = /(\d+)/g;
+    const ide = ident.match(regex);
+    window.location.assign(window.location.href + "?id=" + ide);
+    return;
+  }
   return (
-    <div className="grid-person">
+    <button className="grid-person" onClick={() => clickHandler()}>
       <div className="name-box">{name}</div>
       <div className="item-box">Height: {height}</div>
       <div className="item-box">Birth year: {birth_year}</div>
       <div className="item-box">Eye color: {eye_color}</div>
-      <div className="item-box">Gender{gender}</div>
+      <div className="item-box">Gender: {gender}</div>
+      <div className="item-box">Mass: {mass}</div>
       {/* <div>Fims: {films}</div> */}
       <div className="item-box">Hair color: {hair_color}</div>
       {/* <div className="item-box">Homeworld: {homeworld}</div> */}
       {/* {loading && <div className="loading" />} */}
       <div className="item-box">Skin color: {skin_color}</div>
-    </div>
+    </button>
   );
 }
