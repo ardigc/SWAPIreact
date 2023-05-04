7ea362b631c14825d9c5e5d57dfbba685bde9c79
ardigc <agordo.95@gmail.com>
2023-05-04 19:47:29 +0200

    añadido img gente

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { useNavigate } from "react-router-dom";
+import gente from "./images/gente.jpg";
 
 export default function App() {
   const navigate = useNavigate();
   return (
     <div className="grid grid-cols-3">
-      <button onClick={() => navigate("/people")}>People</button>
+      <button onClick={() => navigate("/people")}>
+        People
+        <img src={gente} />
+      </button>
       <button onClick={() => navigate("/starships")}>Starships</button>
       <button onClick={() => navigate("/planets")}>Planets</button>
       <button onClick={() => navigate("/species")}>Species</button>
