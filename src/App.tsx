e5567f15b8f53e77408a0485b46117e42abbe384
ardigc <agordo.95@gmail.com>
2023-05-04 19:51:02 +0200

    añadido imagenes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import { useNavigate } from "react-router-dom";
 import gente from "./images/gente.jpg";
+import films from "./images/films.jpg";
+import planets from "./images/planets.jpg";
+import ships from "./images/ship.jpg";
+import species from "./images/species.jpg";
+import vehicles from "./images/vehicles.jpg";
 
 export default function App() {
   const navigate = useNavigate();
@@ -9,11 +14,26 @@ export default function App() {
         People
         <img src={gente} />
       </button>
-      <button onClick={() => navigate("/starships")}>Starships</button>
-      <button onClick={() => navigate("/planets")}>Planets</button>
-      <button onClick={() => navigate("/species")}>Species</button>
-      <button onClick={() => navigate("/vehicles")}>Vehicles</button>
-      <button onClick={() => navigate("/films")}>Films</button>
+      <button onClick={() => navigate("/starships")}>
+        Starships
+        <img src={ships} />
+      </button>
+      <button onClick={() => navigate("/planets")}>
+        Planets
+        <img src={planets} />
+      </button>
+      <button onClick={() => navigate("/species")}>
+        Species
+        <img src={species} />
+      </button>
+      <button onClick={() => navigate("/vehicles")}>
+        Vehicles
+        <img src={vehicles} />
+      </button>
+      <button onClick={() => navigate("/films")}>
+        Films
+        <img src={films} />
+      </button>
     </div>
   );
 }
