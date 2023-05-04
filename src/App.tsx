fd946d6bec3164d8c84b8e3dba04ab34e166dead
ardigc <agordo.95@gmail.com>
2023-05-04 20:16:49 +0200

    pantalla de inicio

--- src/App.tsx
+++ src/App.tsx
@@ -9,31 +9,46 @@ import vehicles from "./images/vehicles.jpg";
 export default function App() {
   const navigate = useNavigate();
   return (
-    <div className="grid grid-cols-3">
-      <button onClick={() => navigate("/people")}>
-        People
-        <img src={gente} />
-      </button>
-      <button onClick={() => navigate("/starships")}>
-        Starships
-        <img src={ships} />
-      </button>
-      <button onClick={() => navigate("/planets")}>
-        Planets
-        <img src={planets} />
-      </button>
-      <button onClick={() => navigate("/species")}>
-        Species
-        <img src={species} />
-      </button>
-      <button onClick={() => navigate("/vehicles")}>
-        Vehicles
-        <img src={vehicles} />
-      </button>
-      <button onClick={() => navigate("/films")}>
-        Films
-        <img src={films} />
-      </button>
+    <div className="category-grid">
+      <a className="category-box" onClick={() => navigate("/people")}>
+        <div className="category-title">People</div>
+        <div className="category-image">
+          <img
+            className="flex items-center content-center justify-center"
+            src={gente}
+          />
+        </div>
+      </a>
+      <a className="category-box" onClick={() => navigate("/starships")}>
+        <div className="category-title">Starships</div>
+        <div className="category-image">
+          <img src={ships} />
+        </div>
+      </a>
+      <a className="category-box" onClick={() => navigate("/planets")}>
+        <div className="category-title">Planets</div>
+        <div className="category-image">
+          <img src={planets} />
+        </div>
+      </a>
+      <a className="category-box" onClick={() => navigate("/species")}>
+        <div className="category-title">Species</div>
+        <div className="category-image">
+          <img src={species} />
+        </div>
+      </a>
+      <a className="category-box" onClick={() => navigate("/vehicles")}>
+        <div className="category-title">Vehicles</div>
+        <div className="category-image">
+          <img src={vehicles} />
+        </div>
+      </a>
+      <a className="category-box" onClick={() => navigate("/films")}>
+        <div className="category-title">Films</div>
+        <div className="category-image">
+          <img src={films} />
+        </div>
+      </a>
     </div>
   );
 }
