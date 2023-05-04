7d02a8780418e2edd1019d0f49a0caaa988f9801
ardigc <agordo.95@gmail.com>
2023-05-04 17:54:53 +0200

    poniendo enlaces pantalla principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 export default function App() {
-  return <div>hola</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="grid grid-cols-3">
+      <button onClick={() => navigate("/people")}>People</button>
+      <button onClick={() => navigate("/starships")}>Starships</button>
+      <button onClick={() => navigate("/planets")}>Planets</button>
+      <button onClick={() => navigate("/species")}>Species</button>
+      <button onClick={() => navigate("/vehicles")}>Vehicles</button>
+      <button onClick={() => navigate("/films")}>Films</button>
+    </div>
+  );
 }
