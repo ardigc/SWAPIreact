ca623051b3da29e90a5bb79ce1d56aa6fdebb183
JuanMOlivas <41582680+CerealPlayer@users.noreply.github.com>
2023-05-04 10:10:17 +0000

    Connect to api

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,19 @@
+import { useEffect, useState } from "react";
+
 export default function App() {
-  return <div>Hola que pasa</div>;
+  const [loading, setLoading] = useState(false);
+  const [person, setPerson] = useState<{ name: string }>({ name: "" });
+
+  useEffect(() => {
+    fetch("https://swapi.dev/api/people/1/")
+      .then((res) => res.json())
+      .then((body) => setPerson(body));
+  }, []);
+
+  return (
+    <div>
+      <p>{person.name}</p>
+      {loading && <p>Loading...</p>}
+    </div>
+  );
 }
